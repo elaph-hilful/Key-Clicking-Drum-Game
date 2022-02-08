0a45ec621b8a23f6264625db661dd287237ef186
Hilful <hilful25@gmail.com>
2022-02-09 01:10:46 +0600

    Successful in makeing a function that takes care of both mouse clicks and key board key press

--- index.js
+++ index.js
@@ -1,48 +1,59 @@
 var drumButtons = document.querySelectorAll(".drum").length;
 for(var i = 0 ; i < drumButtons ; i++ )
 {
-    
+ //Detecting Button press of mouse   
 document.querySelectorAll(".drum")[i].addEventListener("click", function (){
 
         var buttonInnerHTML = this. innerHTML;
+        makeSound(buttonInnerHTML);   
     
-    
-            switch (buttonInnerHTML) {
-            case "w":
-                    var audio = new Audio('./sounds/tom-1.mp3');
-                    audio.play();
-                break;
-            case "a":
-                    var audio = new Audio('./sounds/tom-2.mp3');
-                    audio.play();
-                break;
-            case "s":
-                    var audio = new Audio('./sounds/tom-3.mp3');
-                    audio.play();
-                break;
-            case "d":
-                    var audio = new Audio('./sounds/tom-4.mp3');
-                    audio.play();
-                break;
-            case "j":
-                    var audio = new Audio('./sounds/crash.mp3');
-                    audio.play();
-                break;
-            case "k":
-                    var audio = new Audio('./sounds/kick-bass.mp3');
-                    audio.play();
-                break;
-            case "l":
-                    var audio = new Audio('./sounds/snare.mp3');
-                    audio.play();
-                break;
-        
-            default: console.log(buttonInnerHTML);
+            
 
         }
-});
+          );
 }
 
 
 
+//Detecting Keyboard Press
 
+ document.addEventListener("keypress",(event)=>{
+     makeSound(event.key);
+ })
+
+
+
+function makeSound(key){
+    switch (key) {
+        case "w":
+                var audio = new Audio('./sounds/tom-1.mp3');
+                audio.play();
+            break;
+        case "a":
+                var audio = new Audio('./sounds/tom-2.mp3');
+                audio.play();
+            break;
+        case "s":
+                var audio = new Audio('./sounds/tom-3.mp3');
+                audio.play();
+            break;
+        case "d":
+                var audio = new Audio('./sounds/tom-4.mp3');
+                audio.play();
+            break;
+        case "j":
+                var audio = new Audio('./sounds/crash.mp3');
+                audio.play();
+            break;
+        case "k":
+                var audio = new Audio('./sounds/kick-bass.mp3');
+                audio.play();
+            break;
+        case "l":
+                var audio = new Audio('./sounds/snare.mp3');
+                audio.play();
+            break;
+    
+        default: console.log(buttonInnerHTML);
+}
+}
