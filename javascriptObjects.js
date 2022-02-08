1bb03d6ae9bb2c2b768fc7958b3034830c0e0a30
Hilful <hilful25@gmail.com>
2022-02-09 00:12:40 +0600

    Deeper understanding of JavaScript Objects

---
+++ javascriptObjects.js
@@ -0,0 +1,7 @@
+function HouseKeeper(yearsofExperience , name , cleaningRepertoire){
+    this.yearsofExperience = yearsofExperience;
+    this.name = name;
+    this.cleaningRepertoire = cleaningRepertoire;
+}
+// var houseKeeper1 = new HouseKeeper(16,"Tamima",["Lobby","Bedroom"]);
+// var houseKeeper2 = new HouseKeeper(18,"Tazima",["Lobby","Bedroom"]);
