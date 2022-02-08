fcb0ca63de6ab57beb393fb4992d7d960003b945
Hilful <hilful25@gmail.com>
2022-02-08 23:55:00 +0600

    Added single audio to all buttons and also added button background images

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var drumButtons = document.querySelectorAll(".drum").length;
 for(var i = 0 ; i < drumButtons ; i++ )
 {
     
-document.querySelectorAll("button")[i].addEventListener("click", function (){
-    alert("I got Clicked");
+document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+
+    this.style.color = "white";
 });
 }
 
 
 
+
+// var audio = new Audio('./sounds/tom-1.mp3');
+// audio.play();
